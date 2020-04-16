2b634d2d5f677250fd816d76af9009baad3c657b
Mike <email.com>
2020-04-16 19:50:21 +0700

    first cpmponent lesson oleg

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class FirstComponent extends React.Component
+{
+    constructor(props){
+        super(props)
+        this.state = {name: "New component"}
+    }
+
+    
+render(){
+    return (<div><h1>{this.state.name}</h1>
+    <SecondComponent></SecondComponent>
+   
+   
+    </div>)
+}}
+function SecondComponent(){
+    return <h1 align="center">fucntion</h1>;
+}
+ReactDOM.render(<FirstComponent/>,document.getElementById('root'));
---
+++ src/index3.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function SecondComponent(){
+    return <h1 align="center">My componwnr</h1>;
+}
+
+class FirstComponent extends React.Component
+{
+    constructor(props){
+        super(props)
+        this.state = {name: "NEOleg"}
+    }
+
+    
+render(){
+    return (<div><h1>Hello, {this.state.name}</h1>
+    <SecondComponent></SecondComponent>
+    <ThirdComponent></ThirdComponent>
+   
+    </div>)
+}}
+class ThirdComponent extends React.Component
+{
+    constructor(props){
+        super(props)
+        this.state = {name: "Zadanie"}
+    }
+
+    
+render(){
+    return (<div><h4>I zdelal {this.state.name}</h4>
+    
+   
+    </div>)
+}}
+ReactDOM.render(<FirstComponent name="Oleg" />,document.getElementById('root'));
+
